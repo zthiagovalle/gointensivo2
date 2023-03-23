4c71b3a8fe8c3c0f22f11290d2213a2488646acb
Thiago Valle <zthiagovalle@gmail.com>
2023-03-23 20:49:55 -0300

    finalizado consumidor no kafka

---
+++ cmd/consumer/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/zthiagovalle/gointensivo2/internal/infra/database"
+	"github.com/zthiagovalle/gointensivo2/internal/usecase"
+	"github.com/zthiagovalle/gointensivo2/pkg/kafka"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./orders.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	repository := database.NewOrderRepository(db)
+	usecase := usecase.CalculateFinalPrice{OrderRepository: repository}
+	msgChanKafka := make(chan *ckafka.Message)
+
+	topics := []string{"orders"}
+	servers := "host.docker.internal:9094"
+	fmt.Println("Kafka consumer has started")
+	go kafka.Consume(topics, servers, msgChanKafka)
+	kafkaWorker(msgChanKafka, usecase)
+}
+
+func kafkaWorker(msgChan chan *ckafka.Message, uc usecase.CalculateFinalPrice) {
+	fmt.Println("Kafka worker has started")
+	for msg := range msgChan {
+		var OrderInputDTO usecase.OrderInputDTO
+		err := json.Unmarshal(msg.Value, &OrderInputDTO)
+		if err != nil {
+			panic(err)
+		}
+
+		outputDto, err := uc.Execute(OrderInputDTO)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Kafka has processed order %s\n", outputDto.ID)
+	}
+}
