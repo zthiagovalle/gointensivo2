461d3d2e1bd62f632237b4a62c6a1c2ecbe6772a
Thiago Valle <zthiagovalle@gmail.com>
2023-03-23 21:56:19 -0300

    adicionando rabbitmq

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -7,9 +7,11 @@ import (
 
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	_ "github.com/mattn/go-sqlite3"
+	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/zthiagovalle/gointensivo2/internal/infra/database"
 	"github.com/zthiagovalle/gointensivo2/internal/usecase"
 	"github.com/zthiagovalle/gointensivo2/pkg/kafka"
+	"github.com/zthiagovalle/gointensivo2/pkg/rabbitmq"
 )
 
 func main() {
@@ -27,7 +29,17 @@ func main() {
 	servers := "host.docker.internal:9094"
 	fmt.Println("Kafka consumer has started")
 	go kafka.Consume(topics, servers, msgChanKafka)
-	kafkaWorker(msgChanKafka, usecase)
+	go kafkaWorker(msgChanKafka, usecase)
+
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+	msgRabbitmqChannel := make(chan amqp.Delivery)
+
+	go rabbitmq.Consume(ch, msgRabbitmqChannel)
+	rabbitmqWorker(msgRabbitmqChannel, usecase)
 }
 
 func kafkaWorker(msgChan chan *ckafka.Message, uc usecase.CalculateFinalPrice) {
@@ -45,5 +57,23 @@ func kafkaWorker(msgChan chan *ckafka.Message, uc usecase.CalculateFinalPrice) {
 		}
 
 		fmt.Printf("Kafka has processed order %s\n", outputDto.ID)
+	}
+}
+
+func rabbitmqWorker(msgChan chan amqp.Delivery, uc usecase.CalculateFinalPrice) {
+	fmt.Println("Rabbitmq worker has started")
+	for msg := range msgChan {
+		var OrderInputDTO usecase.OrderInputDTO
+		err := json.Unmarshal(msg.Body, &OrderInputDTO)
+		if err != nil {
+			panic(err)
+		}
+
+		outputDto, err := uc.Execute(OrderInputDTO)
+		if err != nil {
+			panic(err)
+		}
+		msg.Ack(false)
+		fmt.Printf("Rabbitmq has processed order %s\n", outputDto.ID)
 	}
 }
